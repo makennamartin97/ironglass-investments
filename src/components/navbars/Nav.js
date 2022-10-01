001faa85cc998996900d999c20e4ef58a3e71ab3
makennamartin97 <makennamartin97@yahoo.com>
2022-09-30 21:16:42 -0400

    changes got undone again

--- src/components/navbars/Nav.js
+++ src/components/navbars/Nav.js
@@ -2,15 +2,8 @@ import React, {useState,useEffect} from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import logo from '../../images/navlogo2.png'
-import {
-    BrowserRouter as Router,
-    Route,
-    Routes,
-    Link,
-    useRouteMatch,
-    useParams
-  } from "react-router-dom";
-  import Routes2 from '../../Routes2.js'
+import {Link} from "react-router-dom";
+import Routes2 from '../../Routes2.js'
 
 const Nav = () => {
 
@@ -27,30 +20,30 @@ const Nav = () => {
         <div className="navbar navbar-admin lg:justify-between">
           <div className="pr-0 w-screen justify-between lg:w-auto">
             {/* igi logo */}
-            <a href="/dashboard">
+            <Link to="/dashboard">
              <img src={logo} alt="..."className="w-32" />
-            </a>
+            </Link>
              {/* hidden menu dropdown for mobile only */}
             <div className="dropdown dropdown-left btn p-0 lg:invisible bg-black text-[#34b2fd]">
                 <label tabIndex={0} className="btn btn-ghost btn-circle">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                 </label>
                 <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow rounded-box w-52 text-black">
-                    <li><a className="btn mr-3 text-blue2 bg-black" href="/dashboard">Dashboard</a></li>
-                    <li><a className="btn mr-3 text-blue2 bg-black" href="/calendly">Calendly</a></li>
-                    <li><a className="btn mr-3 text-blue2 bg-black" href="/events">Events</a></li>
-                    <li><a className="btn mr-3 text-blue2 bg-black" href="/members">Members</a></li>
-                    <li><a className="btn mr-3 text-blue2 bg-black" href="/profile">Settings</a></li>
-                    <li><a className="btn mr-3 text-blue2 bg-black" href="/">Logout</a></li>
+                    <li><Link className="btn mr-3 text-blue2 bg-black" to="/dashboard">Dashboard</Link></li>
+                    <li><Link className="btn mr-3 text-blue2 bg-black" to="/calendly">Calendly</Link></li>
+                    <li><Link className="btn mr-3 text-blue2 bg-black" to="/events">Events</Link></li>
+                    <li><Link className="btn mr-3 text-blue2 bg-black" to="/members">Members</Link></li>
+                    <li><Link className="btn mr-3 text-blue2 bg-black" to="/profile">Settings</Link></li>
+                    <li><Link className="btn mr-3 text-blue2 bg-black" to="/">Logout</Link></li>
                 </ul>
             </div>
           </div>
           {/* navbar row of buttons only on desktop */}
           <ul className="menu menu-horizontal p-0 hidden lg:flex">
-            <li><a href="/dashboard" className='btn mr-3 bg-black text-blue2 blue2-border hover-item-sm orbitron'>Dashboard</a></li>
-            <li><a href="/calendly" className='btn mr-3 bg-black text-blue2 blue2-border hover-item-sm orbitron'>Schedule a Meeting</a></li>
-            <li><a href="/events" className='btn mr-3 bg-black text-blue2 blue2-border hover-item-sm orbitron'>Events</a></li>
-            <li><a href="/members" className='btn mr-3 bg-black text-blue2 blue2-border hover-item-sm orbitron'>Members</a></li>
+            <li><Link to="/dashboard" className='btn mr-3 bg-black text-blue2 blue2-border hover-item-sm orbitron'>Dashboard</Link></li>
+            <li><Link to="/calendly" className='btn mr-3 bg-black text-blue2 blue2-border hover-item-sm orbitron'>Schedule a Meeting</Link></li>
+            <li><Link to="/events" className='btn mr-3 bg-black text-blue2 blue2-border hover-item-sm orbitron'>Events</Link></li>
+            <li><Link to="/members" className='btn mr-3 bg-black text-blue2 blue2-border hover-item-sm orbitron'>Members</Link></li>
             {/* user profile settings and logout dropdown */}
             <div className="dropdown dropdown-end">
               <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
@@ -59,8 +52,8 @@ const Nav = () => {
                 </div>
               </label>
               <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-[#9ca3af] w-52">
-                <li><a className="orbitron active:bg-black" href="/profile">Settings</a></li>
-                <li><a className="orbitron active:bg-black" href="/">Logout</a></li>
+                <li><Link className="orbitron active:bg-black" to="/profile">Settings</Link></li>
+                <li><Link className="orbitron active:bg-black" to="/">Logout</Link></li>
               </ul>
             </div>
           </ul>
--- src/components/navbars/NavbarAuth.js
+++ src/components/navbars/NavbarAuth.js
@@ -1,7 +1,7 @@
 import React,{useState, useEffect,useContext} from 'react';
 import logo from '../../images/navlogo.png'
 import LoginContext from '../../contexts/LoginContext';
-
+import {Link} from "react-router-dom";
 
 
 function NavbarAuth(){
@@ -35,17 +35,17 @@ function NavbarAuth(){
 
       <div className={navbg ? "navbar bg-base-100 navbar-auth active lg:justify-between" : "navbar bg-base-100 navbar-auth lg:justify-between" }>
         <div className="p-7 w-screen justify-between lg:w-auto">
-          <a href="/">
+          <Link to="/">
             <img src={logo} alt="..."className="w-48" />
-          </a>
+          </Link>
 
           <div className="dropdown dropdown-left btn p-0 lg:invisible ml-6 lg:ml-0 bg-black text-[#34b2fd]">
             <label tabIndex={0} className="btn btn-ghost btn-circle">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
             </label>
             <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow rounded-box w-52 text-black">
-              <a className="btn mr-3 text-blue2 bg-black" href="/#about" >About Us</a>
-              <a className="btn mr-3 text-blue2 bg-black" href="/#team" >Our Team</a>
+              <Link className="btn mr-3 text-blue2 bg-black" to="/#about" >About Us</Link>
+              <Link className="btn mr-3 text-blue2 bg-black" to="/#team" >Our Team</Link>
               {loginpage ? (<div className='btn mr-3 text-blue2 bg-black' onClick={showLogin}>Go Back</div>) : (<div className={navbg ?  "btn mr-3 text-black blue2":"btn mr-3 bg-black text-blue2" } onClick={showLogin}>Investor Login</div>)}
             </ul>
           </div>
